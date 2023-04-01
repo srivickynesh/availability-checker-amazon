eed20ef74addc43f769f579c3592174b747014ee
Sri Vignesh <sselvan@redhat.com>
2023-04-01 19:11:31 +0530

    Update availability-check.py

--- availability-check.py
+++ availability-check.py
@@ -1,94 +1,81 @@
-# Python script for Amazon product availability checker
-# importing libraries
-from lxml import html
-import requests
-from time import sleep
-import time
-import schedule
-import smtplib
+try:
+    import requests
+    from bs4 import BeautifulSoup
+    import smtplib
+    import re
 
-# Email id for who want to check availability
-receiver_email_id = "EMAIL_ID_OF_USER"
+except ImportError:
+    print('Some modules are not installed! ')
+    print("try\t\tpip install requests bs4 smtplib")
 
-
-def check(url):
-	headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
-	
-	# adding headers to show that you are
-	# a browser who is sending GET request
-	page = requests.get(url, headers = headers)
-	for i in range(20):
-		# because continuous checks in
-		# milliseconds or few seconds
-		# blocks your request
-		sleep(3)
-		
-		# parsing the html content
-		doc = html.fromstring(page.content)
-		
-		# checking availaility
-		XPATH_AVAILABILITY = '//div[@id ="availability"]//text()'
-		RAw_AVAILABILITY = doc.xpath(XPATH_AVAILABILITY)
-		AVAILABILITY = ''.join(RAw_AVAILABILITY).strip() if RAw_AVAILABILITY else None
-		return AVAILABILITY
-
-	
-def sendemail(ans, product):
-	GMAIL_USERNAME = "YOUR_GMAIL_ID"
-	GMAIL_PASSWORD = "YOUR_GMAIL_PASSWORD"
-	
-	recipient = receiver_email_id
-	body_of_email = ans
-	email_subject = product + ' product availability'
-	
-	# creates SMTP session
-	s = smtplib.SMTP('smtp.gmail.com', 587)
-	
-	# start TLS for security
-	s.starttls()
-	
-	# Authentication
-	s.login(GMAIL_USERNAME, GMAIL_PASSWORD)
-	
-	# message to be sent
-	headers = "\r\n".join(["from: " + GMAIL_USERNAME,
-						"subject: " + email_subject,
-						"to: " + recipient,
-						"mime-version: 1.0",
-						"content-type: text/html"])
-
-	content = headers + "\r\n\r\n" + body_of_email
-	s.sendmail(GMAIL_USERNAME, recipient, content)
-	s.quit()
+global URL
 
 
-def ReadAsin(Asin):
-	# Asin Id is the product Id which
-	# needs to be provided by the user
-	url = "http://www.amazon.in/dp/" + Asin
-	print ("Processing: "+url)
-	ans = check(url)
-	arr = [
-		'Only 1 left in stock.',
-		'Only 2 left in stock.',
-		'In stock.']
-	print(ans)
-	#if ans in arr:
-		# sending email to user if
-		# in case product available
-		sendemail(ans, Asin)
+# enable "allow less secured apps" on your gmail for recieving emails
+def send_mail(URL, sender_email, password, recievers_email, Price):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    # The client sends this command to the SMTP server to identify itself and initiate the SMTP conversation.
+    server.ehlo()
+    server.starttls()  # encrypts the connnection
 
-def job():
-	input_list=["B08CV18TD7"]
-	Asin_list  = input_list.strip().split(",")
-	for p_Asin in Asin_list:
-		print("Tracking....{}".format(p_Asin))
-		ReadAsin(p_Asin)
+    server.login(sender_email, password)
+    subject = 'Price fell down!'
+    body = 'Check the amazon link ' + URL
+    msg = f"Subject: {subject}\n\n{body}"
+    server.sendmail(sender_email, recievers_email, msg)
 
-schedule.every(1).minutes.do(job)
+    print('email has been sent')
+    server.quit()
 
-while True:
-	
-	# running all pending tasks/jobs
-	schedule.run_pending()
-	time.sleep(1)
+
+def information(soup, URL, sender_email, password, recievers_email, Price):
+
+    title = soup.find(id="productTitle").getText().strip()
+    print("\nProduct\t:\n\t", title, "\n")
+    # price1 = soup.find(id="priceblock_ourprice").getText()
+    price = soup.find(id="priceblock_ourprice").get_text().replace(
+        ',', '').replace('₹', '').replace(' ', '').strip()
+    print("Current price\t:\t", price)
+    # print(price)
+    # using regex to convert into float so we can compare with expected price
+    #
+    print("Price you expect\t:\t", Price)
+    if (float(price) < float(Price)):
+        print("YEAH price has fallen!! email will be sent")
+        send_mail(URL, sender_email, password, recievers_email, Price)
+    else:
+        print("seems like you have to wait -) ")
+
+
+def entry():
+    # URL = "https://www.amazon.in/ASUS-i9-10980HK-Graphics-Windows-G532LWS-HF079T/dp/B08HX42DGG/ref=sr_1_1?crid=MM0GWK65DAA4&dchild=1&keywords=asus+rog+32gb+ram+laptop&qid=1609939236&sprefix=rog+32gb+ram+%2Caps%2C494&sr=8-1"
+
+    URL = input("[+] paste the url of the amazon product\t>>\t").strip()
+    Headers = input(
+        "\n[+]just type --my user agent -- in your browser and paste the result \nexampe\n'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36\t>>\t"
+    ).strip()
+    headers = {"User-Agent": Headers}
+    Price1 = input("[+] price you expect\t>>\t").strip()
+    sender_email = input(
+        "\n[+] An email will be sent from this  account(enable allow secured apps on your account)\t>>\t"
+    ).strip()
+    password = input("\n[+] your password\t>>\t").strip()
+    recievers_email = input(
+        "\n[+]  email will be recievedd on this account\t>>\t").strip()
+    page = requests.get(URL, headers=headers)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    Price = Price1.replace(',', '').replace(' ', '').strip()
+
+    # print(soup.prettify)
+    try:
+        print(
+            "# enable --allow less secured apps-- on your gmail if you want to recieve a email"
+        )
+        information(soup, URL, sender_email, password, recievers_email, Price)
+    except AttributeError:
+        print("product info not found")
+
+
+if __name__ == "__main__":
+    entry()
